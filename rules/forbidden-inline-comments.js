2501ba04876da2f57dca770a1f7161c6a3dd6681
Matthias Leitner <matthias.leitner@runtastic.com>
2015-01-14 10:03:34 +0100

    handle quoted hash multine

--- rules/forbidden-inline-comments.js
+++ rules/forbidden-inline-comments.js
@@ -5,6 +5,7 @@ regexes = {
   includesHash: /\#/,
   stringInterpolation: /".*\#{.*"/,
   quotedHash: /['"].*([#]).*['"]/
+  quotedMultilineHash: /(^|\s)['"][^'"]*([#]).*/
 };
 
 module.exports = ForbiddenInlineComments = (function() {
@@ -18,11 +19,12 @@ module.exports = ForbiddenInlineComments = (function() {
   };
 
   ForbiddenInlineComments.prototype.lintLine = function(line, lineApi) {
-    var hashIsAllowed = !regexes.normalComment.test(line) &&
-                        !regexes.stringInterpolation.test(line) &&
-                        !regexes.quotedHash.test(line);
+    var hashIsAllowed = regexes.normalComment.test(line) ||
+                        regexes.stringInterpolation.test(line) ||
+                        regexes.quotedHash.test(line) ||
+                        regexes.quotedMultilineHash.test(line);
 
-    if (regexes.includesHash.test(line) && hashIsAllowed)
+    if (regexes.includesHash.test(line) && !hashIsAllowed)
       return { context: line }
   };
 
